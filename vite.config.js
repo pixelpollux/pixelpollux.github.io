44629d351499c0a9b328041a74bbe7f2d0ec96ca
pixelpollux <terasmash@gmail.com>
2024-08-23 16:33:20 -0500

    trying desperately

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  // base: '/',
+  base: '/taradunmore/',
   server: {
     port: 8080,
     hot: true
